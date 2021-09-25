21e46532523029ad9ab62f8681056837077fd32d
Zach Cyrus <zscyrus31@gmail.com>
2021-09-25 16:09:56 -0400

    Updated test spec file

--- kura_test_repo/cypress/integration/test.spec.js
+++ kura_test_repo/cypress/integration/test.spec.js
@@ -3,7 +3,7 @@ describe('Heading', () => {
         // We need the ip address to run this 
         let ip_address = '3.140.208.112'
 
-        cy.visit(`http://${ip_address}:5000/example-1`)
+        cy.visit(`http://3.140.208.112:5000/example-1`)
 
         cy.get('h1')
             .invoke('text')
