0269dfd905879b79c1b27c867bee5d94a0bbfba6
afashanu3 <84725239+afashanu3@users.noreply.github.com>
2021-09-27 11:50:54 -0400

    Update test.spec.js

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -4,7 +4,7 @@ describe('Heading', () => {
 
         cy.get('h1')
             .invoke('text')
-            .should("equal", "My E Application")
+            .should("equal", "My Awesome Web Application")
     });
 
 });
